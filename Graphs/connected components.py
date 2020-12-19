66533a2f7ec6c2a17211fce4ce5207ede3f3ce63
ANUSREE V A <anuva312@gmail.com>
2020-12-19 12:55:50 +0530

    Added the solution to "Finding connected components"

---
+++ Graphs/connected components.py
@@ -0,0 +1,39 @@
+class Graph:
+    def __init__(self,n):
+        self.adj_list = {}
+        self.visited = {}
+        # Initializing adjacency list of each vertex into an empty list
+        for i in range(1,n+1):
+            self.visited[i] = 0
+            self.adj_list[i] = []
+
+    def initialize_adj_list(self, m):
+        # Filling the adjacency list
+        for _ in range(m):
+            a,b = map(int, input().split())
+            self.adj_list[a].append(b)
+            self.adj_list[b].append(a)
+
+    def reach(self, x):
+        self.visited[x] = 1
+        for node in self.adj_list[x]:
+            if not self.visited[node]:
+                self.reach(node)
+
+    def dfs(self):
+        # Initializing no.of connected components to 0
+        cc=0
+        for node in self.adj_list.keys():
+            if not self.visited[node]:
+                self.reach(node)
+                cc+=1
+        return cc
+
+
+if __name__ == '__main__':
+    # Taking input : no.of vertices, edges
+    n, m = map(int, input().split())
+    maze = Graph(n)
+    maze.initialize_adj_list(m)
+    print(maze.dfs())
+
