a27410c8e6daa5b69bea52a03a7069ec5670cc84
ANUSREE V A <anuva312@gmail.com>
2020-12-19 12:51:07 +0530

    Added solution to new problem "Finding an exit from maze"

---
+++ Graphs/exit from maze.py
@@ -0,0 +1,42 @@
+class Graph:
+    def __init__(self,n):
+        self.adj_list = {}
+        self.visited = {}
+        self.dest = 0
+        self.result = 0
+        # Initializing adjacency list of each vertex into an empty list
+        for i in range(1,n+1):
+            self.visited[i] = 0
+            self.adj_list[i] = []
+
+    def initialize_adj_list(self, m):
+        # Filling the adjacency list
+        for _ in range(m):
+            a,b = map(int, input().split())
+            self.adj_list[a].append(b)
+            self.adj_list[b].append(a)
+
+    # def get_dest(self,d):
+    #     self.dest = d
+
+    def reach(self, x):
+        self.visited[x] = 1
+        if self.dest in self.adj_list[x]:
+            self.result = 1
+            return
+        # Depth First Search
+        # print(x, self.adj_list)
+        for node in self.adj_list[x]:
+            if not self.visited[node]:
+                self.reach(node)
+
+
+if __name__ == '__main__':
+    # Taking input : no.of vertices, edges
+    n, m = map(int, input().split())
+    maze = Graph(n)
+    maze.initialize_adj_list(m)
+    src, maze.dest = map(int, input().split())
+    # maze.get_dest(dest)
+    maze.reach(src)
+    print(maze.result)
