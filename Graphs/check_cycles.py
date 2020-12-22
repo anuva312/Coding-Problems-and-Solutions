d9546843ed60f7c5628ecc452656c0bee5eda15a
ANUSREE V A <anusreeva93@gmail.com>
2020-12-22 23:11:13 +0530

    Removed unnecessary lines of code

--- Graphs/check_cycles.py
+++ Graphs/check_cycles.py
@@ -4,30 +4,24 @@ class Graph:
     def __init__(self,n):
         self.adj_list = {}
         self.visited = {}
+        # To keep a list of nodes explored, to find cycles
+        #  If coming back to the same node, a cycle is found
         self.in_path = {}
         self.ifcyclic = 0
-        # self.rev_adj_list = {}
-        # self.clock = 0
         # Initializing adjacency list of each vertex into an empty list
         for i in range(1,n+1):
             self.visited[i] = 0
             self.adj_list[i] = []
             self.in_path[i] = 0
-        # self.post = {}
-        # self.prev = {}
-        # self.max_post = 0
 
     def initialize_adj_list(self, m):
         # Filling the adjacency list
         for _ in range(m):
             a,b = map(int, input().split())
-            # self.rev_adj_list[b].append(a)
             self.adj_list[a].append(b)
 
     def explore(self,x):
         # Exploring the graph in depth first order
-        # self.prev[x] = self.clock
-        # self.clock+=1
         self.visited[x] = 1
         self.in_path[x] = 1
         for node in self.adj_list[x]:
@@ -36,8 +30,6 @@ class Graph:
             elif self.in_path[node]:
                 self.ifcyclic = 1
         self.in_path[x] = 0
-        # self.post[x]= self.clock
-        # self.clock+=1
 
     def dfs(self):
         # Initializing no.of connected components to 0
